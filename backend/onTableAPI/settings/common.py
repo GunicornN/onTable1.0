19fd460e7bb01b2c44daa011c01665c0adb37618
aDubanchet <>
2021-05-16 14:29:37 +0200

    zzz

--- backend/onTableAPI/settings/common.py
+++ backend/onTableAPI/settings/common.py
@@ -156,8 +156,8 @@ USE_L10N = True
 USE_TZ = True
 
 # MEDIA FILES
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
-MEDIA_ROOT = '/vol/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 MEDIA_URL = '/media/'
 
 
